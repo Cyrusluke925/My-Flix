8b5722d459aa54352bbd6ed05545c72624ae8832
rajmahal <rmahal25@gmail.com>
2018-08-30 10:45:58 -0700

    Search with director and title

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,26 +14,71 @@ const end = "&query=Jack+Reacher";
         let title = encodeURI(formArr[0].value);
         console.log("Parsed Value: "+title);
         
+        console.log(formArr.length);
+        console.log("Director value: "+(encodeURI(formArr[3].value)));
+
+        if(formArr[3].value !== "" || formArr[3].value !== null ){
+            console.log("Condition met");
+            let director = encodeURI(formArr[3].value);
+
+            var myURL = "https://api.themoviedb.org/3/search/multi?api_key="+apiKey+"&language=en-US&query="+director+"&page=1&include_adult=false";
+
+            var exampleURL = "https://api.themoviedb.org/3/search/multi?api_key=e6104cb8ac4b63d1e99b6c905b41870c&language=en-US&query=Jean-Marc%20Vall%C3%A9e&page=1&include_adult=false";
+
+            $.ajax({
+                method: 'GET',
+                url: exampleURL,
+                success: directorSuccess,
+                error: directorError
+            
+            });
+
+            function directorSuccess(response){
+                
+
+                let result = response.results[0].known_for.forEach( filmTitle => {
+                    if(formArr[0].value === filmTitle.title){
+                        console.log(filmTitle);
+                    }
+
+                });
+
+                
+                console.log("No results found")
+
+            }
+
+            function directorError (e1, e2, e3) {
+                console.log(e1)
+                console.log(e2)
+                console.dir(e3)  
+            }
 
 
-
-        $.ajax({
-            method: 'GET',
-            url: "https://api.themoviedb.org/3/search/multi?api_key="+apiKey+"&language=en-US&query="+title+"&page=1&include_adult=false" ,
-            success: onSuccess,
-            error: onError
-        
-        });
-        
-        function onSuccess (response) {
-            console.log(response);
+        }else{
+            //Title Search
+            $.ajax({
+                method: 'GET',
+                url: "https://api.themoviedb.org/3/search/multi?api_key="+apiKey+"&language=en-US&query="+title+"&page=1&include_adult=false" ,
+                success: onSuccess,
+                error: onError
+            
+            });
+            
+            function onSuccess (response) {
+                console.log(response);
+            }
+            
+            function onError (e1, e2, e3) {
+                console.log(e1)
+                console.log(e2)
+                console.dir(e3)  
+            }
         }
-        
-        function onError (e1, e2, e3) {
-            console.log(e1)
-            console.log(e2)
-            console.dir(e3)  
-        }
+
+
+
+
 
     });
 
