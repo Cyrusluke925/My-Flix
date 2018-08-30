a2bfcfe15fb3a9b777d0349717e14968cb13fac1
Luke <luke@Lukes-MacBook-Pro.local>
2018-08-29 17:14:30 -0700

    styled landing page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 $( document ).ready(function() {
 //api key
-const apiKey = config.API_KEY;
-const api_endpoint = 'https://api.themoviedb.org/3/authentication/token/new?api_key='+apiKey+"&query=Jack+Reacher";
+const apiKey = 'e6104cb8ac4b63d1e99b6c905b41870c';
+const api_endpoint = 'https://api.themoviedb.org/3/authentication/token/new?api_key='+ apiKey
 
 
 $.ajax({
@@ -29,38 +29,33 @@ $.ajax({
 
 
 
-/*
+
+  //when mouse hovers over log in option on Landing Page (Index.html)
+
+$('.logIn').on('mouseenter', function() {
+  $('.signUp').css('filter', 'brightness(50%)');
+  $('.logIn').css('filter', 'brightness(120%)');
+})
+
+$('.logIn').on('mouseleave', function() {
+  $('.signUp').css('filter', 'brightness(90%)')
+  $('.logIn').css('filter', 'brightness(100%)')
+})
 
 
 
+//when mouse hovers over sign up option on Landing page (Index.html)
 
-DO NOT TOUCH
-
-
-
-
-
-
+$('.signUp').on('mouseenter', function() {
+  $('.signUp').css('filter', 'brightness(130%)');
+  $('.logIn').css('filter', 'brightness(50%');
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('.signUp').on('mouseleave', function() {
+  $('.logIn').css('filter', 'brightness(100%)')
+  $('.signUp').css('filter', 'brightness(90%)')
+})
 
 
 
@@ -71,7 +66,30 @@ DO NOT TOUCH
 
 
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
