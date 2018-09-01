d44f796fbe3037a760eb9988769139c543d06612
rajmahal <rmahal25@gmail.com>
2018-09-01 14:44:32 -0700

    Login working without BCRYPT

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('./models');
 
-
+//var salt = bcrypt.genSaltSync();
+var salt = bcrypt.genSaltSync();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -94,7 +95,7 @@ app.post('/signup', (req,res)=>{
         if (users.length >= 1) {
             return res.status(401).json({message: 'Username already used please try again'})
         }else{
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
+            bcrypt.hash(req.body.password, salt, (err, hash) => {
                 if(err){ 
                   console.log("hashing error:", err);
                   
@@ -104,8 +105,12 @@ app.post('/signup', (req,res)=>{
 
                     let userToCreate = {
                         userName: username,
-                        password: hash
+                        //commenting out hashed pass saving as text
+                        //password: hash
+                        password: password
                     }
+                    console.log("Signup User: "+username);
+                    console.log("Signup Hash: "+hash);
 
                     db.User.create( userToCreate ,(err, users) => {
                         if(err){console.log(err);}
@@ -131,64 +136,63 @@ app.post('/signup', (req,res)=>{
                 });
             }
         });
-});
+    });
 
 app.post('/login', (req, res) => {
     console.log("LOGIN CALLED");
     var username = req.body.userName;
-    // console.log(username)
-    // console.log(req.body)
+    var password = req.body.password;
+    console.log("Username: "+username);
+    console.log("Password: "+password);
 
     // FINDS THE USER IN THE DATABASE
     db.User.find({userName: username}, (err, users) => {
-        if (users.length < 1) {
+        if(err){
+            console.log(err);
+        }else if (users.length < 1) {
             return res.status(401).json({message: 'Username/Password incorrect'})
         }
+        console.log("users found: "+users);
 
-        if(req.body.password !== users[0].password) {
-            return res.status(401).json({message: 'Username/Password incorrect'})
-        }
-        
-        if(req.body.password === users[0].password) {
-    
-                // create a json web token
-                const token = jwt.sign({
-                    // add some identifying information
-                    userName: users[0].userName,
-                    _id: users[0]._id
-                    }, 
-                // add our super secret key (which should be hidden, not plaintext like this)
-                "vampires",
-                // these are options, not necessary
-                {
-                    // its good practice to have an expiration amount for jwt tokens.
-                    expiresIn: "1h"
-                },
-                );
-                // send success back to user, along with a token.
-                res.status(200).json(
-                
-                {
-                    message: 'Auth successful',
-                    token
-                }
-                )
-            // the password provided does not match the password on file.
-            } else {
-                res.status(401).json({message: "Username/Password incorrect"})
-            }
-            })
+        // if(bcrypt.compare(password, users[0].password, (err, hash) => {
+        //     //console.log("Got to Hasing");
+
+        //     if(err){ 
+        //         console.log("hashing error:", err); 
+        //         return res.status(401).json({message: 'Username/Password incorrect'})
+        //     }
+        // })){
+
+        if(password === users[0].password){
+                //console.log("coming in: "+hash);
+                //console.log("found pass: "+users[0].password);
+                    jwt.sign(
+                        { username },
+                        "vampires",
+                        {
+                            expiresIn: "1h"
+                        },
+                        (err, signedJwt) => {
+                            if(err){console.log(err);}
+                            console.log(signedJwt);
+                            res.status(200).json({
+                                message: 'User Created',
+                                username,
+                                signedJwt
+                            })
+
+                        });
+            
+                    }
+        })
 });
         
-
-
-    
 
 
 
     app.post('/verify', verifyToken, (req, res) => {
         let verified= jwt.verify(req.token, 'vampires')
-        console.log("verified: ", verified)
+        //console.log("verified: ", verified)
         res.json(verified)
       })
 
@@ -215,34 +219,6 @@ app.post('/login', (req, res) => {
           res.sendStatus(403);
         }
       }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
